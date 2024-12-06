35708aef0e37594e4291efceb3cbfb9d40f984b7
youngjin0305 <blueati35@naver.com>
2024-12-06 16:53:10 +0900

    add requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory, send_file
+from flask import Flask, request, jsonify, send_from_directory
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from bson import ObjectId
